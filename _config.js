7c863a7689ffb3853a9b065cc731e658c238c035
amwayiallan <amwayiallan24@gmail.com>
2024-04-12 21:16:54 +0300

    Added a Jenkinsfile

--- _config.js
+++ _config.js
@@ -4,6 +4,6 @@ var config = {}
 config.mongoURI = {
     production: 'mongodb+srv://amwayiallan:Vigilante85@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://amwayiallan:Vigilante85@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    test: 'mongodb+srv://amwayiallan:Vigilante85@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
